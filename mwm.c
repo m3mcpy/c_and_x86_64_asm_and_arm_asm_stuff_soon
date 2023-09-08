4adc84c48d0a4ec3deb3ad606199b4b85f9de5cf
m3mcpy <98256610+m3mcpy@users.noreply.github.com>
2023-09-07 20:34:27 -0400

    Add files via upload

--- mwm.c
+++ mwm.c
@@ -2,11 +2,27 @@
 int main(void) {
   Display *display = XOpenDisplay(NULL);
   XEvent event;
+  size_t current = 0;
+  size_t length = 0;
   XGrabKey(display, XKeysymToKeycode(display, XStringToKeysym("Q")), Mod1Mask, DefaultRootWindow(display), True, GrabModeAsync, GrabModeAsync);
+  XGrabKey(display, XKeysymToKeycode(display, XStringToKeysym("J")), Mod1Mask, DefaultRootWindow(display), True, GrabModeAsync, GrabModeAsync);
+  XGrabKey(display, XKeysymToKeycode(display, XStringToKeysym("K")), Mod1Mask, DefaultRootWindow(display), True, GrabModeAsync, GrabModeAsync);
   while (1) {
     if (event.xkey.keycode == XKeysymToKeycode(display, XStringToKeysym("Q"))) {
       XCloseDisplay(display);
       return 1;
+    } else if (event.xkey.keycode == XKeysymToKeycode(display, XStringToKeysym("J"))) {
+      if (current > 0) {
+        current--;
+      } else {
+        current = length;
+      }
+    } else if (event.xkey.keycode == XKeysymToKeycode(display, XStringToKeysym("K"))) {
+      if (current < length) {
+        current++;
+      } else {
+        current = 0;
+      }
     }
   }
 }
