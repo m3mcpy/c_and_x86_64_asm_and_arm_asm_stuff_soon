d462d8c86950729644f6a3371c534d6a64a6e8d0
m3mcpy <98256610+m3mcpy@users.noreply.github.com>
2023-09-07 20:45:46 -0400

    Add files via upload

--- mwm.c
+++ mwm.c
@@ -1,14 +1,19 @@
 #include <X11/Xlib.h>
+#include <stdlib.h>
 int main(void) {
   Display *display = XOpenDisplay(NULL);
+  Window root = DefaultRootWindow(display);
   XEvent event;
   size_t current = 0;
   size_t length = 0;
+  Window *windows = NULL;
   XGrabKey(display, XKeysymToKeycode(display, XStringToKeysym("Q")), Mod1Mask, DefaultRootWindow(display), True, GrabModeAsync, GrabModeAsync);
   XGrabKey(display, XKeysymToKeycode(display, XStringToKeysym("J")), Mod1Mask, DefaultRootWindow(display), True, GrabModeAsync, GrabModeAsync);
   XGrabKey(display, XKeysymToKeycode(display, XStringToKeysym("K")), Mod1Mask, DefaultRootWindow(display), True, GrabModeAsync, GrabModeAsync);
+  XGrabKey(display, XKeysymToKeycode(display, XStringToKeysym("Return")), Mod1Mask, DefaultRootWindow(display), True, GrabModeAsync, GrabModeAsync);
   while (1) {
     if (event.xkey.keycode == XKeysymToKeycode(display, XStringToKeysym("Q"))) {
+      free(windows);
       XCloseDisplay(display);
       return 1;
     } else if (event.xkey.keycode == XKeysymToKeycode(display, XStringToKeysym("J"))) {
@@ -23,6 +28,10 @@ int main(void) {
       } else {
         current = 0;
       }
+    } else if (event.xkey.keycode == XKeysymToKeycode(display, XStringToKeysym("Return"))) {
+      Window window = XCreateSimpleWindow(display, root, 100, 100, 400, 300, 1, BlackPixel(display, 0), WhitePixel(display, 0));
+      XMapWindow(display, window);
+      windows = realloc(windows, (length + 1) * sizeof(Window));
     }
   }
 }
