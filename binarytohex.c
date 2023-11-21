7fd88d7941ba8cd3d71083f6d8b21b8a7360eb4f
m3mcpy <98256610+m3mcpy@users.noreply.github.com>
2023-11-21 06:00:03 -0500

    Add files via upload

---
+++ binarytohex.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(int argc, char **argv) {
+  if (argc == 2) {
+    size_t size = 1;
+    size_t sum = 0;
+    for (size_t i = strlen(argv[1]) - 1; i != -1; i--) {
+      if (argv[1][i] == '1') {
+        sum += size;
+      } else if (argv[1][i] != '0') {
+        return 0;
+      }
+      size *= 2;
+    }
+    char *hex = NULL;
+    size_t hexsize = 0;
+    while (sum != 0) {
+      char mode = 0;
+      char value = sum % 16;
+      switch (sum % 16) {
+      case 10:
+        value = 'a';
+        break;
+      case 11:
+        value = 'b';
+        break;
+      case 12:
+        value = 'c';
+        break;
+      case 13:
+        value = 'd';
+        break;
+      case 14:
+        value = 'e';
+        break;
+      case 15:
+        value = 'f';
+        break;
+      default:
+        value += '0';
+        break;
+      }
+      hex = realloc(hex, hexsize + 1);
+      hex[hexsize++] = value;
+      sum /= 16;
+    }
+    for (size_t i = strlen(hex); i != -1; i--) {
+      printf("%c", hex[i]);
+    }
+    printf("\n");
+  }
+  return 0;
+}
